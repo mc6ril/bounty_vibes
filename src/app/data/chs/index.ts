161e3e4dd0dc4f12f1c9252ab7278703e3b4303b
SUPADM <supadm@altilabo.lan>
2025-02-26 12:27:16 +0100

    Modifications du contenu des TS

--- src/app/data/chs/index.ts
+++ src/app/data/chs/index.ts
@@ -1,43 +1,142 @@
 export const characters = {
-  glat: { name: "Ahsoka Tano", tag: ["Leader", "Galactic Legend", "Spectre", "Unaligned Force User"] },
-  ashokaSnips: { name: "Ahsoka Tano (Snips)", tag: ["Leader", "501st", "Galactic Republic", "Jedi"] },
-  cat: { name: "Commander Ahsoka Tano", tag: ["Scoundrel", "Unaligned Force User", "Galactic Republic"] },
-  ezraBridgerExil: { name: "Ezra Bridger (Exile)", tag: ["Spectre", "Jedi"] },
-  generalKenobi: { name: "General Kenobi", tag: ["Leader", "Fleet Commander", "Galactic Republic", "Jedi"] },
-  generalSyndulla: { name: "General Syndulla", tag: ["Leader", "Spectre"] },
-  huyang: { name: "Huyang", tag: ["Spectre", "Droid"] },
-  jmk: { name: "Jedi Master Kenobi", tag: ["Leader", "Galactic Legend", "Galactic Republic", "Jedi"] },
-  padawanSabine: { name: "Padawan Sabine Wren", tag: ["Spectre", "Mandalorian", "Unaligned Force User"] },
-  padmeAmidala: { name: "Padme Amidala", tag: ["Leader", "Galactic Republic"] },
-  cls: { name: "Commander Luke Skywalker", tag: ["Leader", "Rebel", "Unaligned Force User"] },
-  chewbacca: { name: "Chewbacca", tag: ["Smuggler", "Rebel", "Scoundrel", "Wookiee"] },
-  c3po: { name: "C-3PO", tag: ["Droid", "Rebel", "Galactic Republic", "Resistance"] },
-  hansolo: { name: "Han Solo", tag: ["Mercenary", "Rebel", "Scoundrel", "Smuggler"] },
-  threepio: { name: "Threepio & Chewie", tag: ["Rebel", "Droid", "Wookiee"] },
-  jml: { name: "Jedi Master Luke Skywalker", tag: ["Galactic Legend", "Leader", "Jedi"] },
-  jkl: { name: "Jedi Knight Luke Skywalker", tag: ["Leader", "Jedi"] },
-  revan: { name: "Jedi Knight Revan", tag: ["Old Republic", "Leader", "Jedi"] },
-  jkck: { name: "Jedi Knight Cal Kestis", tag: ["Mercenary", "Leader", "Jedi"] },
-  hyoda: { name: "Hermit Yoda", tag: ["Jedi"] },
-  reva: { name: "Third Sister", tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"] },
-  gi: { name: "Grand Inquisitor", tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"] },
-  cinqfrere: { name: "Fifth Brother", tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"] },
-  huitfrere: { name: "Eighth Brother", tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"] },
-  septsoeur: { name: "Seventh Sister", tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"] },
+  glat: {
+    name: "Ahsoka Tano",
+    tag: ["Leader", "Galactic Legend", "Spectre", "Unaligned Force User"],
+  },
+  ashokaSnips: {
+    name: "Ahsoka Tano (Snips)",
+    tag: ["Leader", "501st", "Galactic Republic", "Jedi"],
+  },
+  cat: {
+    name: "Commander Ahsoka Tano",
+    tag: ["Scoundrel", "Unaligned Force User", "Galactic Republic"],
+  },
+  ezraBridgerExil: {
+    name: "Ezra Bridger (Exile)",
+    tag: ["Spectre", "Jedi"],
+  },
+  generalKenobi: {
+    name: "General Kenobi",
+    tag: ["Leader", "Fleet Commander", "Galactic Republic", "Jedi"],
+  },
+  generalSyndulla: {
+    name: "General Syndulla",
+    tag: ["Leader", "Spectre"],
+  },
+  huyang: {
+    name: "Huyang",
+    tag: ["Spectre", "Droid"],
+  },
+  jmk: {
+    name: "Jedi Master Kenobi",
+    tag: ["Leader", "Galactic Legend", "Galactic Republic", "Jedi"],
+  },
+  padawanSabine: {
+    name: "Padawan Sabine Wren",
+    tag: ["Spectre", "Mandalorian", "Unaligned Force User"],
+  },
+  padmeAmidala: {
+    name: "Padme Amidala",
+    tag: ["Leader", "Galactic Republic"],
+  },
+  cls: {
+    name: "Commander Luke Skywalker",
+    tag: ["Leader", "Rebel", "Unaligned Force User"],
+  },
+  chewbacca: {
+    name: "Chewbacca",
+    tag: ["Smuggler", "Rebel", "Scoundrel", "Wookiee"],
+  },
+  c3po: {
+    name: "C-3PO",
+    tag: ["Droid", "Rebel", "Galactic Republic", "Resistance"],
+  },
+  hansolo: {
+    name: "Han Solo",
+    tag: ["Mercenary", "Rebel", "Scoundrel", "Smuggler"],
+  },
+  threepio: {
+    name: "Threepio & Chewie",
+    tag: ["Rebel", "Droid", "Wookiee"],
+  },
+  jml: {
+    name: "Jedi Master Luke Skywalker",
+    tag: ["Galactic Legend", "Leader", "Jedi"],
+  },
+  jkl: {
+    name: "Jedi Knight Luke Skywalker",
+    tag: ["Leader", "Jedi"],
+  },
+  revan: {
+    name: "Jedi Knight Revan",
+    tag: ["Old Republic", "Leader", "Jedi"],
+  },
+  jkck: {
+    name: "Jedi Knight Cal Kestis",
+    tag: ["Mercenary", "Leader", "Jedi"],
+  },
+  hyoda: {
+    name: "Hermit Yoda",
+    tag: ["Jedi"],
+  },
+  reva: {
+    name: "Third Sister",
+    tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"],
+  },
+  gi: {
+    name: "Grand Inquisitor",
+    tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"],
+  },
+  cinqfrere: {
+    name: "Fifth Brother",
+    tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"],
+  },
+  huitfrere: {
+    name: "Eighth Brother",
+    tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"],
+  },
+  septsoeur: {
+    name: "Seventh Sister",
+    tag: ["Leader", "Empire", "Inquisitorius", "Unaligned Force User"],
+  },
   jabba: {
     name: "Jabba the Hutt",
     tag: ["Leader", "Galactic Legend", "Bounty Hunter", "Hutt Cartel", "Smuggler"],
   },
-  boba: { name: "Boba Fett", tag: ["Leader", "Scoundrel", "Bounty Hunter", "Hutt Cartel"] },
-  boushh: { name: "Boushh (Leia Organa)", tag: ["Leader", "Scoundrel", "Bounty Hunter", "Hutt Cartel"] },
+  boba: {
+    name: "Boba Fett",
+    tag: ["Leader", "Scoundrel", "Bounty Hunter", "Hutt Cartel"],
+  },
+  boushh: {
+    name: "Boushh (Leia Organa)",
+    tag: ["Leader", "Scoundrel", "Bounty Hunter", "Hutt Cartel"],
+  },
   krrsantan: {
     name: "Krrsantan",
     tag: ["Leader", "Scoundrel", "Bounty Hunter", "Hutt Cartel", "Mercenary", "Wookiee"],
   },
-  skifflando: { name: "Skiff Guard (Lando Calrissian)", tag: ["Scoundrel", "Smuggler", "Hutt Cartel"] },
-  aphra: { name: "Doctor Aphra", tag: ["Leader", "Mercenary", "Scoundrel"] },
-  ooo: { name: "0-0-0", tag: ["Droid", "Scoundrel"] },
-  bt1: { name: "BT-1", tag: ["Droid", "Scoundrel"] },
-  ig88: { name: "IG-88", tag: ["Leader", "Scoundrel", "Bounty Hunter", "Droid"] },
-  sonde: { name: "Imperial Probe Droid", tag: ["Empire", "Droid"] },
+  skifflando: {
+    name: "Skiff Guard (Lando Calrissian)",
+    tag: ["Scoundrel", "Smuggler", "Hutt Cartel"],
+  },
+  aphra: {
+    name: "Doctor Aphra",
+    tag: ["Leader", "Mercenary", "Scoundrel"],
+  },
+  ooo: {
+    name: "0-0-0",
+    tag: ["Droid", "Scoundrel"],
+  },
+  bt1: {
+    name: "BT-1",
+    tag: ["Droid", "Scoundrel"],
+  },
+  ig88: {
+    name: "IG-88",
+    tag: ["Leader", "Scoundrel", "Bounty Hunter", "Droid"],
+  },
+  sonde: {
+    name: "Imperial Probe Droid",
+    tag: ["Empire", "Droid"],
+  },
 };
